1593f668cc08d5d0bd951c4f94c46c9048e6409d
Steven Tanner <sctanner@email.sc.edu>
2022-12-09 15:46:57 -0500

    Script edits and BattleScene

--- Assets/Scripts/BattleInfo.cs
+++ Assets/Scripts/BattleInfo.cs
@@ -1,23 +1,23 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
 
-// public class BattleInfo : MonoBehaviour
-// {
-//     public Text name; //name of player or insectosaur
-//     public Slider health; //health bar 
+ public class BattleInfo : MonoBehaviour
+ {
+     public Text name; //name of player or insectosaur
+     public Slider health; //health bar 
 
-//     public void setHUD(/*Units unit*/) 
-//     {
-//         // nameText.text = unit.unitsName; //unit name in text box
-//         // health.maxVal = unit.maxHealth; //max health for player
-//         // health.value = unit.currHealth; //current health for player
-//     }
+     public void setHUD(Units unit) 
+     {
+          name.text = unit.unitsName; //unit name in text box
+          health.maxValue = unit.maxHealth; //max health for player
+          health.value = unit.currHealth; //current health for player
+     }
 
-//     //sets health
-//     public void setHealth(int health)
-//     {
-//         health.value = health;
-//     }
-// }
+     //sets health
+     public void setHealth(int hp)
+     {
+         health.value = hp;
+     }
+ }
--- Assets/Scripts/BattleSystems.cs
+++ Assets/Scripts/BattleSystems.cs
@@ -1,152 +1,152 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
-// public class BattleSystems : MonoBehaviour
-// {
-//     public GameObject PlayerPrefab;
-//     public GameObject EnemyPrefab;
+public class BattleSystems : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public GameObject EnemyPrefab;
 
-//     public Transform playerBattleStation;
-//     public Transform enemyBattleStation;
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
 
-//     //Units playerUnit;
-//     //Units enemyUnit;
+    Units playerUnit;
+    Units enemyUnit;
     
-//     //public Text dialogueText;
-//     //for the dialogue box in battle ADD when finished
+    public Text dialogueText;
+    //for the dialogue box in battle ADD when finished
 
-//     public BattleInfo playerInfo;
-//     public BattleInfo enemyInfo;
+    public BattleInfo playerInfo;
+    public BattleInfo enemyInfo;
 
-//     public BattleState state;
+    public BattleState state;
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         state = BattleState.START;
-//         StartCoroutine(SetupBattle());
-//     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
 
-//     //sets up battle 
-//     IEnumerator SetupBattle()
-//     {
-//         GameObject playerGO = Instantiate(PlayerPrefab, playerBattleStation);
-//         playerUnit = playerGO.GetComponent<Units>();
-//         GameObject enemyGO = Instantiate(EnemyPrefab, enemyBattleStation);
-//         enemyUnit = enemyGO.GetComponent<Units>();
+    //sets up battle 
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGO = Instantiate(PlayerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Units>();
+        GameObject enemyGO = Instantiate(EnemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Units>();
 
-//         //dialogueText.text = enemyUnit.unitsName;
-//         //unit name in text box
+        dialogueText.text = enemyUnit.unitsName;
+        //unit name in text box
 
-//         playerInfo.setHUD(playerUnit);
-//         enemyInfo.setHUD(enemyUnit);
+        playerInfo.setHUD(playerUnit);
+        enemyInfo.setHUD(enemyUnit);
 
-//         yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(3f);
 
-//         state = BattleState.PLAYERTURN;
-//         PlayerTurn();
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
 
-//     }
+    }
 
-//     //attack system
-//     IEnumerator PlayerAttack()
-//     {
-//         //damage enemy
-//         bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+    //attack system
+    IEnumerator PlayerAttack()
+    {
+        //damage enemy
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
 
-//         enemyInfo.setHealth(enemyUnit.currHealth);
-//         //dialogueText.text = "Attack successful";
+        enemyInfo.setHealth(enemyUnit.currHealth);
+        dialogueText.text = "Attack successful";
 
-//         yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
 
-//         if(isDead){
-//             //end battle
-//             state = BattleState.WON;
-//             EndBattle();
-//         }
-//         else {
-//             //enemy turn
-//             state = BattleState.ENEMYTURN;
-//             StartCoroutine(EnemyTurn());
-//         }
-//     }
+        if(isDead){
+            //end battle
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else {
+            //enemy turn
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
 
-//     IEnumerator EnemyTurn()
-//     {
-//         //dialogueText.text = enemyUnit.unitsName + "is attacking";
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitsName + "is attacking";
 
-//         yield return new WaitForSeconds(1f); //waits a second
+        yield return new WaitForSeconds(1f); //waits a second
 
-//         //bool isDead playerUnit.TakeDamage(enemyUnit.damage); //damage the player
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage); //damage the player
 
-//         playerInfo.setHealth(playerUnit.currHealth); 
+        playerInfo.setHealth(playerUnit.currHealth); 
 
-//         yield return new WaitForSeconds(1f); //wait again
+        yield return new WaitForSeconds(1f); //wait again
 
-//         //check if dead or not and change states
-//         if(isDead) {
-//             state = BattleState.LOST; //lost
-//             EndBattle();
-//         }
-//         else {
-//             state = BattleState.PLAYERTURN; //player turn
-//             PlayerTurn();
-//         }
-//     }
+        //check if dead or not and change states
+        if(isDead) {
+            state = BattleState.LOST; //lost
+            EndBattle();
+        }
+        else {
+            state = BattleState.PLAYERTURN; //player turn
+            PlayerTurn();
+        }
+    }
 
-//     //end of battle scene
-//     void EndBattle() 
-//     {
-//         if(state == BattleState.WON) {
-//             //dialogueText.text = "You won!";
-//         }
-//         else if(state == BattleState.LOST) {
-//             //dialogueText.text = "You were defeated.";
-//         }
-//     }
+    //end of battle scene
+    void EndBattle() 
+    {
+        if(state == BattleState.WON) {
+            dialogueText.text = "You won!";
+        }
+        else if(state == BattleState.LOST) {
+            dialogueText.text = "You were defeated.";
+        }
+    }
 
-//     //player chooses an action
-//     void PlayerTurn()
-//     {
-//         //dialogueText.text = "Choose an action";
-//     }
+    //player chooses an action
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action";
+    }
 
-//     //heals player
-//     IEnumerator PlayerHeal() 
-//     {
-//         playerUnit.Heal(10); //heals 10 health
+    //heals player
+    IEnumerator PlayerHeal() 
+    {
+        playerUnit.Heal(10); //heals 10 health
 
-//         playerInfo.setHealth(playerUnit.currHealth);
-//         //dialogueText.text = "Healed!";
+        playerInfo.setHealth(playerUnit.currHealth);
+        dialogueText.text = "Healed!";
 
-//         yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
 
-//         state = BattleState.ENEMYTURN;
-//         StartCoroutine(EnemyTurn());
-//     }
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
 
-//     //attack button
-//     public void OnAttackButton()
-//     {
-//         //checks if its players turn
-//         if(state != BattleState.PLAYERTURN)
-//             return;
+    //attack button
+    public void OnAttackButton()
+    {
+        //checks if its players turn
+        if(state != BattleState.PLAYERTURN)
+            return;
 
-//         StartCoroutine(PlayerAttack());
-//     }
+        StartCoroutine(PlayerAttack());
+    }
 
-//     //heal button
-//     public void OnHealButton()
-//     {
-//         //checks if its players turn
-//         if(state != BattleState.PLAYERTURN)
-//             return;
+    //heal button
+    public void OnHealButton()
+    {
+        //checks if its players turn
+        if(state != BattleState.PLAYERTURN)
+            return;
 
-//         StartCoroutine(PlayerHeal());
-//     }
+        StartCoroutine(PlayerHeal());
+    }
 
-// }
+}
